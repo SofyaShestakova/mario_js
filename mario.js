3e71b3daee3305be70193b70673480bc240be55a
SofyaShestakova <soneshya@mail.ru>
2020-07-23 19:35:27 +0500

    add style.css

--- mario.js
+++ mario.js
@@ -2,7 +2,7 @@ function printPyramid(height){
     for(var i = 1; i<height+1; i++){
         let content  = ' '.repeat(height-i) +'#'.repeat(i+1);
         let newElement = document.createElement("p");
-        newElement.innerHTML =''.repeat(height-1) + content;
+        newElement.innerHTML = content;
         let pyramid = document.getElementById("pyramid");
         pyramid.appendChild(newElement);
     }
