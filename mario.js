495b795763036b9f4ac5245b1b01df94f03e3b3a
SofyaShestakova <soneshya@mail.ru>
2020-08-25 16:36:22 +0300

    some changes

--- mario.js
+++ mario.js
@@ -2,8 +2,8 @@ function printPyramid(height){
     for(var i = 1; i<height+1; i++){
         let content  = ' '.repeat(height-i) +'#'.repeat(i+1);
         let newElement = document.createElement("p");
-        newElement.innerHTML = content;
         let pyramid = document.getElementById("pyramid");
+        newElement.innerHTML = content;
         pyramid.appendChild(newElement);
     }
     document.getElementById("construction").remove();
